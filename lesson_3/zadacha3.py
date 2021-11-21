c00f1d4599a8a5f60c8a6fa9cf7d1bb9db0a3c2c
Sergey Potapenko <potapenkosps@gmail.com>
2021-11-21 06:48:49 +0200

    v_1

--- lesson_3/zadacha3.py
+++ lesson_3/zadacha3.py
@@ -1,12 +1,11 @@
 # TODO Выcокостный год
-print("Введите год")
+print("Введите год для проверки")
 year = int(input(""))
-if (year > 1900) and (year < 1000000):
-    print("Заданный год верный")
-else:
-    print("Заданный год не верный")
-
-if (year % 4 == 0) and (year % 100 == 0) and (year % 400 == 0):
+if year < 1900:
+    print("Введеный год не отвечает условиям")
+elif year > 1000000:
+    print("Введеный год не отвечает условиям")
+elif (year % 4 == 0) and (year % 100 == 0) and (year % 400 == 0):
     print(year, "Год высокостный")
 else:
     print(year, "Год не высокостный")
