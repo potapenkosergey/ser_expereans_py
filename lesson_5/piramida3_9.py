9ea7b109ac65a2296db3bbc4f1c7017692518971
potapenkosergey <potapenkosps@gmail.com>
2021-12-03 23:30:09 +0200

    v_1

--- lesson_5/piramida3_9.py
+++ lesson_5/piramida3_9.py
@@ -1,13 +1,15 @@
 # TODO Pyramid
-meaning = int(input(" Введите число от 3 до 9: "))
-# Проверка на правильность введеного числа
-if (meaning < 10) and (meaning >= 3):
-    # Основной цикл программы
-    for i in range(1, meaning + 1):
-        for j in range(1, i + 1):
-            print(j, end="")
-        for j in range(i - 1, 0, -1):
-            print(j, end="")
-        print()
-else:
-    print("Введенное число не соответствет диапазону")
+try:
+    number = int(input(" Введите число от 3 до 9: "))
+    # Проверка на правильность введеного числа
+    if (number < 10) and (number >= 3):
+        for i in range(1, number + 1):
+            for j in range(1, i + 1):
+                print(j, end="")
+            for j in range(i - 1, 0, -1):
+                print(j, end="")
+            print()
+    else:
+        print(" Введенное число не соответствет диапазону ")
+except:
+    print(" Введите чило а не букву ")
